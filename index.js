7cd6079db7746d21ac64fde4e9bd54f5a5e1eb04
DiegoBuitrago <alejobq@hotmail.com>
2020-11-16 21:45:33 -0500

    indexNew

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log('htbgfdcsxcvbgethbgv')
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(bodyParser.text())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})      
+
+app.post('/myparam', (req, res) => {
+  var id = req.param('id')
+  var name = req.param('name')
+  console.log('ID:  '+ id + '  NAME:  ' + name)
+  res.send('ID:  '+ id + '  NAME:  ' + name)
+});
+
+app.post('/myheaders', (req, res) => {
+  var id = req.headers.id
+  var name = req.headers.name
+  console.log('ID:  '+ id + '  NAME:  ' + name)
+  res.send('ID:  '+ id + '  NAME:  ' + name)
+});
+
+app.post('/mybody', (req, res) => {
+  console.log(req.body)
+  res.status(200).send(req.body)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
