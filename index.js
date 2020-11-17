0afd0cff0083fd551fd3a3426734dedbdb4d94c6
DiegoBuitrago <alejobq@hotmail.com>
2020-11-16 23:08:13 -0500

    Ultimo push

--- index.js
+++ index.js
@@ -32,8 +32,23 @@ app.get('/second_service', (req, res) => {
 })    
 
 app.get('/third_service', (req, res) => {
-    //console.log(req.body)
-    res.status(200).send('Tercer servicio')
+    res.json([
+        {
+          "name": "Diego Buitrago",
+          "age": 25,
+          "driving_license": true
+        },
+        {
+          "name": "Diego Guerrero",
+          "age": 90,
+          "driving_license": true
+        },
+        {
+           "name": "Laura Salinas",
+           "age": 22,
+           "driving_license": false
+        }
+      ])
 })
 
 app.listen(port, () => {
