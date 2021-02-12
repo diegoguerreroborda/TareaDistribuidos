0e1e8037a8b7706a9d23b8d5d19b000d2c90f053
diegoguerreroborda <diegoguerreroborda@gmail.com>
2021-02-11 20:29:08 -0500

    sd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
 const cors = require('cors')
-const port = 3000
+const port = process.argv[2];
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -13,32 +13,18 @@ var dataClient = "";
 
 app.get('/', (req, res) => {
   res.send('ok')
-})    
-
-app.get('/info_client', (req, res) => {
-  res.send(dataClient)
-}) 
+})  
 
 app.post('/data_client', (req, res) => {
-  console.log(req.body)
-  dataClient = req.body
+  console.log(`Entrante es ${req.body}.`)
+  dataClient = req.body;
+  res.status(200);
 });
 
-app.post('/myheaders', (req, res) => {
-  var id = req.headers.id
-  var name = req.headers.name
-  console.log('ID:  '+ id + '  NAME:  ' + name)
-  res.send('ID:  '+ id + '  NAME:  ' + name)
-});
-
-app.get('/first_service', (req, res) => {
-  res.status(200).json({"list" : [{ username: 'Flavio'}, 
-                                  {username : 'Diego', age: 32}, 
-                                  {username : 'Jason', age : 12 }],
-                      "numberList": 3,
-                      "Hola" : "Mundo"
-  })
-})
+app.get('/info_client', (req, res) => {
+  console.log(`Dataclient es ${dataClient.img}.`);
+  res.send(dataClient);
+}) 
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
